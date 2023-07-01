7028b3a836dc49fdc8fca8ede419ce8b1332eb9f
sakoharu <135291091+sakoharu@users.noreply.github.com>
2023-07-01 11:58:12 +0000

    [Finish]Aboutページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   get 'books/new'
   post 'books' => 'books#create'
   get 'books/index'
   get 'books/show'
   get 'books/edit'
   get 'homes/top' => 'homes#top'
-  get 'about' => 'homes#about'
+  get 'homes/about' => 'homes#about', as: 'about'
   devise_for :users
   root to: "homes#top"
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
