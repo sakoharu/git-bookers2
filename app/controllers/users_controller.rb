1c0f09e28fab16d2b0a90bcef8b562b2f0739323
sakoharu <135291091+sakoharu@users.noreply.github.com>
2023-07-06 10:43:58 +0000

    [Finish]28

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images 
+    # @post_images = @user.post_images
   end
-  
+
   def edit
   end
-  
+
   def index
-  end 
-end 
+  end
+end
