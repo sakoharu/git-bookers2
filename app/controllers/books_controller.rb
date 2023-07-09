ed36bfa6c705727c82dbc50c4aac8f7789a8d519
sakoharu <135291091+sakoharu@users.noreply.github.com>
2023-07-09 09:41:54 +0000

    [Finish]33

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
+  
   def edit
-    
+     book = Book.find(params[:id])
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "homes#top"
-  resources :books, only: [:edit, :index, :show, :destroy, :create]
+  resources :books, only: [:edit, :index, :show, :destroy, :create, :update]
   resources :users, only: [:show, :edit, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
