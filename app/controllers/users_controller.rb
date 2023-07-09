ff7589c087779214bceb62cad0575cc3e71f1562
sakoharu <135291091+sakoharu@users.noreply.github.com>
2023-07-09 12:11:42 +0000

    [Finish]36

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,12 @@ class UsersController < ApplicationController
   def show
     @book_new = Book.new
     @user = User.find(params[:id])
-    @book = Book.find(params[:id])
+    @books = @user.books
 
   end
 
   def edit
-    @user = current_user
+    @user = User.find(params[:id])
   end
 
   def index
