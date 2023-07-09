85e180b287367d3eb0837bd33680c9a39af8e20a
sakoharu <135291091+sakoharu@users.noreply.github.com>
2023-07-09 10:02:42 +0000

    [Finish]34

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,13 @@
 class BooksController < ApplicationController
-  
+
   def edit
-     book = Book.find(params[:id])
+     @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path
   end
 
   def index
