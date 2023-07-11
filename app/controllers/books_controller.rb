334ed1057e298be66f001c2019fd29ea58517227
sakoharu <135291091+sakoharu@users.noreply.github.com>
2023-07-11 05:44:54 +0000

    [Finish]53

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
 
   def edit
      @book = Book.find(params[:id])
+     
   end
 
   def update
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,9 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    unless @user.id == current_user.id
+    redirect_to users_path
+    end
   end
 
   def index
