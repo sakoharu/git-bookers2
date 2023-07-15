630abd0f07d8459470a859e8027c2fabfc732fc3
sakoharu <135291091+sakoharu@users.noreply.github.com>
2023-07-15 07:44:24 +0000

    [Finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
     unless @user.id == current_user.id
-    redirect_to 
+    redirect_to user_path(current_user.id)
     end
   end
 
