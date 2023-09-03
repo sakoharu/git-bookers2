225a04282315ab96e37e899c91a53336f2252ca2
sakoharu <135291091+sakoharu@users.noreply.github.com>
2023-09-03 05:28:27 +0000

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,8 @@ class UsersController < ApplicationController
     if @user.save
     flash[:notice] = "You have updated user successfully."
     redirect_to user_path(@user.id)
-  else
-    render :edit
+  　else
+    　render :edit
     end
   end
 
